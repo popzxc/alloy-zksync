bff3083a15917a7541727993e61b075da0ff9539
vasyl-ivanchuk <vasyl.ivanchuk@gmail.com>
2025-02-24 07:25:21 +0000

    Deploying to gh-pages from @ popzxc/alloy-zksync@52262d463dd3d5c1d6251cd3966310b531700851 🚀

---
+++ alloy_zksync/contracts/l1/bridge_hub/Bridgehub/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["BridgehubCalls","BridgehubEvents"],"fn":["new"],"struct":["BridgehubInstance","NewPriorityRequest","l2TransactionBaseCostCall","l2TransactionBaseCostReturn","requestL2TransactionDirectCall","requestL2TransactionDirectReturn","requestL2TransactionTwoBridgesCall","requestL2TransactionTwoBridgesReturn"]};
---
+++ trait.impl/alloy_provider/provider/trait/trait.Provider.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"alloy_zksync":[["impl&lt;P&gt; Provider&lt;<a class=\"struct\" href=\"alloy_zksync/network/struct.Zksync.html\" title=\"struct alloy_zksync::network::Zksync\">Zksync</a>&gt; for <a class=\"struct\" href=\"alloy_zksync/provider/layers/anvil_zksync/struct.AnvilZKsyncProvider.html\" title=\"struct alloy_zksync::provider::layers::anvil_zksync::AnvilZKsyncProvider\">AnvilZKsyncProvider</a>&lt;P&gt;<div class=\"where\">where\n    P: Provider&lt;<a class=\"struct\" href=\"alloy_zksync/network/struct.Zksync.html\" title=\"struct alloy_zksync::network::Zksync\">Zksync</a>&gt;,</div>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

33a0d5ee717a080fab424e02c1a27af5663963dd
popzxc <popzxc@yandex.ru>
2025-03-12 12:58:41 +0000

    Deploying to gh-pages from @ popzxc/alloy-zksync@807f998401f4374e99eb2c5d468eaf29dcba5b70 🚀

---
+++ trait.impl/alloy_consensus/block/header/trait.BlockHeader.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"alloy_zksync":[["impl BlockHeader for <a class=\"struct\" href=\"alloy_zksync/network/header/struct.Header.html\" title=\"struct alloy_zksync::network::header::Header\">Header</a>"],["impl BlockHeader for <a class=\"struct\" href=\"alloy_zksync/network/header_response/struct.HeaderResponse.html\" title=\"struct alloy_zksync::network::header_response::HeaderResponse\">HeaderResponse</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ trait.impl/core/ops/drop/trait.Drop.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"alloy_zksync":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.81.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"alloy_zksync/node_bindings/struct.AnvilZKsyncInstance.html\" title=\"struct alloy_zksync::node_bindings::AnvilZKsyncInstance\">AnvilZKsyncInstance</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

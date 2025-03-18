71cb2c6f8514819ba4d8ea398aea3744cc38e770
Daniyar Itegulov <ditegulov@gmail.com>
2025-03-18 14:34:35 +1100

    chore: adapt for alloy 0.12.5

--- src/network/unsigned_tx/eip712/mod.rs
+++ src/network/unsigned_tx/eip712/mod.rs
@@ -368,9 +368,6 @@ impl SignableTransaction<Signature> for TxEip712 {
 }
 
 impl RlpEcdsaEncodableTx for TxEip712 {
-    #[doc = " The default transaction type for this transaction."]
-    const DEFAULT_TX_TYPE: u8 = TxType::Eip712 as u8;
-
     #[doc = " Calculate the encoded length of the transaction\'s fields, without a RLP"]
     #[doc = " header."]
     fn rlp_encoded_fields_length(&self) -> usize {

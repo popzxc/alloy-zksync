141460d2fc85cac9297724b290b88da844d17b74
popzxc <popzxc@yandex.ru>
2025-06-26 09:48:52 +0000

    Deploying to gh-pages from @ popzxc/alloy-zksync@3dfdfe3d5074ca8f9162faa8c6e0e2324e3ceaaa 🚀

---
+++ trait.impl/alloy_provider/builder/trait.ProviderLayer.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"alloy_zksync":[["impl&lt;P&gt; ProviderLayer&lt;P, <a class=\"struct\" href=\"alloy_zksync/network/struct.Zksync.html\" title=\"struct alloy_zksync::network::Zksync\">Zksync</a>&gt; for <a class=\"struct\" href=\"alloy_zksync/provider/layers/anvil_zksync/struct.AnvilZKsyncLayer.html\" title=\"struct alloy_zksync::provider::layers::anvil_zksync::AnvilZKsyncLayer\">AnvilZKsyncLayer</a><div class=\"where\">where\n    P: Provider&lt;<a class=\"struct\" href=\"alloy_zksync/network/struct.Zksync.html\" title=\"struct alloy_zksync::network::Zksync\">Zksync</a>&gt;,</div>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
